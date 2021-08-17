5114fb307a5a8558e375d9a6912ced3af1613279
eric369java <55117363+eric369java@users.noreply.github.com>
2021-08-17 10:49:46 -0700

    Improved export

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Trello-To-Microsoft-Planner
 
 ## Step 1: 
-In Trello, `Show Menu > More > Print and Report > Export as JSON`. Copy the JSON dump into a file called trello.json. Save in working directory. 
+In Trello, `Show Menu > More > Print and Report > Export as JSON`. Copy the JSON dump into a JSON file. Save the file to the working directory. 
 
 ## Step 2: 
-In the working directory make sure Python 3, `pandas`, and `xlsxwriter` are installed. Creatr an empty Excel file called trello.xlsx in the working directory. Run `py -m trello`. 
+In the working directory make sure Python 3, `pandas`, and `xlsxwriter` are installed (`py -m pip install pandas` and `py -m pip install xlsxwriter`. May need to put `python`, `python3`, or `py3` instead of `py`). Create an empty Excel file called trello.xlsx in the working directory. Run `py -m trello [json] [table]`. Instead of `[json]` write the name of the json file saved previously. Instead of `[table]` type the name you want for the Excel sheet. For example: `py -m trello Informatics.json Informatics`.  
 
 ## Step 3:
-In Sharepoint, create a list from trello.xlsx. 
+In the Informatics Sharepoint, create a new List. Select Create from Excel file > Upload > choose trello.xlsx. 
 
 ## Step 4: 
 In Power Automate, set up a Flow to read the Excel file and create tasks & buckets. The current flow looks like
 ```
-Get Tasks from Sharepoint Lists:
+Get items from Sharepoint Lists:
 For each item: 
     If Created field of task is True:
         Create a new Bucket 
@@ -20,7 +20,16 @@ For each item:
     For each bucket: 
         If task belongs in the bucket:
             Create a new task in the bucket.
+List all Tasks
+For each task: 
+    For each item: 
+        If item Title matches task Title: 
+            Update task details with description and checklists. 
 ```
+Flows may need slight modification on its import/export location. Go to Edit flow > Get Items > Change fields Site Address and List Name to the right Sharepoint location and the right list (different for each department). Then in Apply to each 3 > Create bucket, Apply to each 3 > List Buckets, and List tasks, make sure the Group Id and Plan Id are pointing to the plan you want to export to. 
 
 ## Step 5:
 Run the Flow in Power Automate. 
+
+## Step 6:
+Play a chess game. By the time you're done the Flow will finish running (it's really slow)
--- trello.py
+++ trello.py
@@ -1,11 +1,14 @@
 import json 
 import pandas as pd 
+import io 
+import sys
 
-def parse_cards_data(): 
-    with open('trello.json', 'r') as json_file:
+def parse_cards_data(file_name): 
+    with io.open(file_name, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
-        cards = data['cards']
-        
+        cards = data['cards']        
+        checks = data['checklists']
+
         lists_data = []
         lists = data['lists']
         for _list in lists:
@@ -21,23 +24,29 @@ def parse_cards_data():
 
         cards_data = []
         for card in cards:
+            
+            if card['closed'] == True: 
+                continue 
+
             bucket = None 
             priority = None
             new_bucket = 'False' 
-            emails = [] 
+            emails = []
+            colors = ['No'] * 10 
 
+            #calculate priority of the task
             try:  
-                if card['labels'][0]['color'] == 'green':
+                if '3' in card['labels'][0]['name']:
                     priority = 5 
-                elif card['labels'][0]['color'] == 'orange': 
+                elif 'Important' in card['labels'][0]['name'] or '2' in card['labels'][0]['name']: 
                     priority = 3
-                elif card['labels'][0]['color'] == 'red': 
+                elif 'Urgent' in card['labels'][0]['name'] or '1' in card['labels'][0]['name']: 
                     priority = 1 
                 else: 
                     priority = 9 
             except IndexError:
                 priority = 9 
-
+            #check if new bucket needs to be created
             for _list in lists_data: 
                 if _list['id'] == card['idList']:
                     bucket = _list['name']
@@ -45,8 +54,8 @@ def parse_cards_data():
                         _list['created'] = True 
                         new_bucket = 'True'
             if bucket == None:
-                continue 
-  
+                continue     
+            #add users to task
             for id in card['idMembers']: 
                 for member in data['members']: 
                     if member['id'] == id: 
@@ -61,54 +70,117 @@ def parse_cards_data():
             while len(emails) < 5: 
                 emails.append(None)
 
+            #if card has cover, use color of cover; if has both cover & label color, use cover color; if only label, use label color. 
+            try: 
+                card_color = None 
+                if card['cover']['color'] == None: 
+                    card_color = card['labels'][0]['color']
+                else: 
+                    card_color = card['cover']['color'] 
+
+                if card_color == 'green': 
+                    colors[0] = 'Yes' 
+                elif card_color == 'yellow': 
+                    colors[1] = 'Yes'
+                elif card_color == 'orange': 
+                    colors[2] = 'Yes'
+                elif card_color == 'red': 
+                    colors[3] = 'Yes'
+                elif card_color == 'purple': 
+                    colors[4] = 'Yes' 
+                elif card_color == 'blue': 
+                    colors[5] = 'Yes' 
+                elif card_color == 'sky': 
+                    colors[6] = 'Yes'
+                elif card_color == 'lime': 
+                    colors[7] = 'Yes'
+                elif card_color == 'pink': 
+                    colors[8] = 'Yes'
+                elif card_color == 'black': 
+                    colors[9] = 'Yes'
+            except IndexError: 
+                pass 
+
             _card = { 
                 "Title": card['name'],
                 "Priority": priority, 
                 "Due Date Time": card['due'], 
                 "Bucket Name": bucket,
-                "New Bucket": new_bucket, 
+                "New Bucket": new_bucket,
+                "Description": card['desc'].rstrip(),  
                 "Email 1" : emails[0],
                 "Email 2" : emails[1], 
                 "Email 3" : emails[2], 
-                "Email 4" : emails[3],
-                "Email 5" : emails[4] 
+                "Green": colors[0],
+                "Yellow": colors[1], 
+                "Orange": colors[2],
+                "Red": colors[3],
+                "Purple": colors[4], 
+                "Dark Blue": colors[5], 
+                "Light Blue": colors[6], 
+                "Turqoise": colors[7],
+                "Pink": colors[8],
+                "Black": colors[9]
             }
+
+            #get up to 15 checklist items
+            names = [None] * 15
+            states = [None] * 15
+            i = 0 
+            for check in checks: 
+                for check_id in card['idChecklists']: 
+                    if check['id'] == check_id: 
+                        for j in check['checkItems']:
+                            names[i] = j['name'] 
+                            if j['state'] == 'complete': 
+                                states[i] = 'Yes'
+                            else:
+                                states[i] = 'No'
+                            i += 1
+ 
+            for i in range(15): 
+                _card[f'checkItem{i}Name'] = names[i]
+                _card[f'checkItem{i}State'] = states[i]
 
             cards_data.append(_card)
         
     return cards_data 
 
-def write_excel(data): 
+def write_excel(data, sheet_name): 
 
-    Titles = [ i['Title'] for i in data]
-    Priorities = [i['Priority'] for i in data]
-    Due = [i['Due Date Time'] for i in data]
-    Bucket = [i['Bucket Name'] for i in data]
-    First = [i['New Bucket'] for i in data] 
-    Email1 = [i['Email 1'] for i in data]
-    Email2 = [i['Email 2'] for i in data]
-    Email3 = [i['Email 3'] for i in data]
-    Email4 = [i['Email 4'] for i in data]
-    Email5 = [i['Email 5'] for i in data]
+    data_frame = { 
+        "Title": [i['Title'] for i in data],
+        "Priority": [i['Priority'] for i in data], 
+        "Due Date Time": [i['Due Date Time'] for i in data], 
+        "Bucket Name": [i['Bucket Name'] for i in data],
+        "New Bucket": [i['New Bucket'] for i in data],
+        "Description": [i['Description'] for i in data],  
+        "Email 1" : [i['Email 1'] for i in data],
+        "Email 2" : [i['Email 2'] for i in data], 
+        "Email 3" : [i['Email 3'] for i in data], 
+        "Green": [i['Green'] for i in data],
+        "Yellow": [i['Yellow'] for i in data], 
+        "Orange": [i['Orange'] for i in data],
+        "Red": [i['Red'] for i in data],
+        "Purple": [i['Purple'] for i in data], 
+        "Dark Blue": [i['Dark Blue'] for i in data], 
+        "Light Blue": [i['Light Blue'] for i in data], 
+        "Turqoise": [i['Turqoise'] for i in data],
+        "Pink": [i['Pink'] for i in data],
+        "Black": [i['Black'] for i in data]
+    }
 
-    df = pd.DataFrame({
-        'Title': Titles,
-        'Priority': Priorities,
-        'Due Date Time': Due, 
-        'Bucket Name': Bucket, 
-        'Create': First, 
-        'Email1' : Email1,
-        'Email2' : Email2, 
-        'Email3' : Email3,
-        'Email4' : Email4, 
-        'Email5' : Email5 
-    })
+    for i in range(15): 
+        data_frame[f'checkItem{i}Name'] = [j[f'checkItem{i}Name'] for j in data]
+        data_frame[f'checkItem{i}State'] = [j[f'checkItem{i}State'] for j in data] 
+
+    df = pd.DataFrame(data_frame)
 
     writer = pd.ExcelWriter('trello.xlsx', engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Informatics', startrow=1, header=False, index=False)
+    df.to_excel(writer, sheet_name=sheet_name, startrow=1, header=False, index=False)
 
     workbook = writer.book
-    worksheet = writer.sheets['Informatics']
+    worksheet = writer.sheets[sheet_name]
 
     (max_row, max_col) = df.shape 
 
@@ -120,5 +192,7 @@ def write_excel(data):
     writer.save()
 
 if __name__ == '__main__':
-    data = parse_cards_data()
-    write_excel(data)
+    file_name = sys.argv[1]
+    sheet_name = sys.argv[2]
+    data = parse_cards_data(file_name)
+    write_excel(data, sheet_name)
